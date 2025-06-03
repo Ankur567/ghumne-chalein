66a3fba31c53e6f1d2c81a93283b9279938c5252
Ankur Manna <suchitrak5plus@gmail.com>
2025-06-03 12:26:44 +0530

    added request

--- backend/app.py
+++ backend/app.py
@@ -111,6 +111,6 @@ def getOwnerDetails():
 def checkTripRequested(tripId):
     return check_trip_requested_by_user(tripId)
 
-port = int(os.environ.get("PORT", 5002))
+port = int(os.environ.get("PORT", 5000))
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=port)
